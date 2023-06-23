c388a54074c3ccea6e42fb5d2889fec05c9df0fc
a.trofimov <a.trofimov@vlife.kz>
2023-06-23 09:43:47 +0600

    spring init

---
+++ src/main/java/com/great_table/great_table/GreatTableApplication.java
@@ -0,0 +1,13 @@
+package com.great_table.great_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/great_table/great_table/GreatTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.great_table.great_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
